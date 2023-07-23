ee5771c749b0c70f62a4fb0d61e577819015eb07
mohammadalsmadi2000 <mohammadalsmadi.fcbayern@gmail.com>
2023-07-23 11:58:24 +0000

    feat: 📝 Add Footer Component

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return (<div style={{ width: '100%', height: '120px' }} className="flex items-center justify-start pl-4 align-top bg-green-500">
+        &copy; 2023
+    </div>)
+}
